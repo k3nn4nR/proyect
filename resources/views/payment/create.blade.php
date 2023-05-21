0550e369ff3eb7e8812c14573a77d58e5bc287e0
kennan <kennan_11@hotmail.com>
2023-05-21 22:32:38 +1000

    first dev commit

--- resources/views/payment/create.blade.php
+++ resources/views/payment/create.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Payment')
 
 @section('plugins.Select2', true)
+@section('plugins.SweetAlert2', true)
 
 @section('content_header')
     <h1 class="m-0 text-dark">Payment Create</h1>
@@ -61,10 +62,11 @@
                             <table class="table responsive">
                                 <thead>
                                     <tr>
-                                        <th>All <input type="checkbox" id="select-all"></th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>E-Mail</th>
+                                        <th><input type="checkbox" id="select-all"></th>
+                                        <th>Description</th>
+                                        <th>Ammount</th>
+                                        <th>Price</th>
+                                        <th>Sub-total</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -75,8 +77,9 @@
                                                 <option>Choose One..</option>
                                             </select>
                                         </td>
-                                        <td><input type="number" name="items_ammount[]"></td>
-                                        <td><input type="number" name="items_quantity[]"></td>
+                                        <td><input type="number" name="items_ammount[]" onchange="updateSubtotal($(this),this)"></td>
+                                        <td><input type="number" name="items_price[]" onchange="updateSubtotal($(this),this)"></td>
+                                        <td><input type="number" disabled ></td>
                                     </tr>
                                     <tr>
                                         <td><input type="checkbox" id="select-row"></td>
@@ -85,8 +88,9 @@
                                                 <option>Choose One..</option>
                                             </select>
                                         </td>
-                                        <td><input type="number" name="services_ammount[]"></td>
-                                        <td><input type="number" name="services_quantity[]"></td>
+                                        <td><input type="number" name="services_ammount[]" onchange="updateSubtotal($(this),this)"></td>
+                                        <td><input type="number" name="services_price[]" onchange="updateSubtotal($(this),this)"></td>
+                                        <td><input type="number" disabled ></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -137,33 +141,30 @@
             getItems(headers)
             getServices(headers)
 
-            //add new row for service 
-            //add new row for item
-            //use select for items with name="items"
-            //use select for services with name="services"
-
-            // Add new row
+            // Add new row for item
             $("#add-item-row").click(function(){
                 $(".table tbody tr").last().after(
                     '<tr class="fadetext">'+
                         '<td><input type="checkbox" id="select-row"></td>'+
                         '<td><select class="form-control" id="services_select" name="items[]"><option>Choose One..</option></select></td>'+
                         '<option>Choose One..</option></select></td>'+
-                        '<td><input type="number" name="items_ammount[]"></td>'+
-                        '<td><input type="number" name="items_quantity[]"></td>'+
+                        '<td><input type="number" name="items_ammount[]" onchange="updateSubtotal($(this))></td>'+
+                        '<td><input type="number" name="items_price[]" onchange="updateSubtotal($(this))></td>'+
+                        '<td><input type="number" disabled ></td>'+
                     '</tr>'
                 );
             })
 
-            // Add new row
+            // Add new row for service 
             $("#add-service-row").click(function(){
                 $(".table tbody tr").last().after(
                     '<tr class="fadetext">'+
                         '<td><input type="checkbox" id="select-row"></td>'+
                         '<td><select class="form-control" id="items_select" name="services[]"><option>Choose One..</option></select></td>'+
                         '<option>Choose One..</option></select></td>'+
-                        '<td><input type="number" name="services_ammount[]"></td>'+
-                        '<td><input type="number" name="services_quantity[]"></td>'+
+                        '<td><input type="number" name="services_ammount[]" onchange="updateSubtotal($(this))></td>'+
+                        '<td><input type="number" name="services_price[]" onchange="updateSubtotal($(this))></td>'+
+                        '<td><input type="number" disabled ></td>'+
                     '</tr>'
                 );
             })
@@ -188,14 +189,47 @@
                     var isChecked = $(this).find('input[type="checkbox"]').is(":checked");
                     var tableSize = $(".table tbody tr").length;
                     if(tableSize == 1){
-                        alert('All rows cannot be deleted.');
+                        Swal.fire(
+                            'Error!',
+                            'At least 1 row is expected!',
+                            'error'
+                        )
                     }else if(isChecked){
                         $(this).remove();
                     }
                 });
             });
-
         });
+
+        function updateSubtotal(object,value){
+            var headRow = object.parent().siblings()
+            var rowCount = 0;
+            var cRow = headRow;  // used as a reference to the current row
+            console.log(cRow.nextSibling)
+            while (cRow = cRow.nextSibling)  // While there is a next sibling, loop
+            {
+                console.log(cRow)
+                // if (cRow.tagName == "TR" && cRow.style.display === "")
+                //     rowCount++;      // increment rowCount by 1
+                if(cRow.nextSibling.tagName === 'items_ammount[]')
+                    console.log(object.siblings(".items_price[]"))
+            }
+            // console.log(value.name)
+            // console.log('value: '+value.value)
+            // var sibling;
+            // if(value.name === 'items_ammount[]')
+            //     console.log(object.siblings(".items_price[]"))
+            // if(value.name === 'items_price[]')
+            //     console.log(object.siblings(".items_ammount[]"))
+            // if(value.name === 'services_price[]')
+            //     console.log(object.siblings(".services_ammount[]"))
+            // if(value.name === 'services_ammount[]')
+            //     console.log(object.siblings(".services_price[]"))
+        }
+
+        function updateTotal(){
+
+        }
 
         function getCompanies(headers){
             $.ajax({
